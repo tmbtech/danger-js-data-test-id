db5a903dfcf481bdf2a185433731d5d8bf8293f3
Robbie dela Victoria <robbie.dela.victoria@gmail.com>
2025-09-23 16:07:49 -0700

    feat(example): change data-testid again to trigger Danger on modified file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
     <main>
-      <h1 data-testid="greeting-updated">Hello Danger</h1>
-      <p>Changed test id to trigger Danger.</p>
+      <h1 data-testid="greeting-updated-again">Hello Danger</h1>
+      <p>Changed test id again to trigger Danger on non-new-file.</p>
     </main>
   );
 }
